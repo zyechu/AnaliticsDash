8bfa1cf7402a53cfd4024980431e70a08c7a1153
zyechu <matt96@wp.pl>
2021-01-16 10:40:09 +0100

    some commit

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import plotly.express as px
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
-import matplotlib.pyplot as plt
+# import numpy as np
+# import plotly.express as px
+# from plotly.subplots import make_subplots
+# import plotly.graph_objects as go
+# import matplotlib.pyplot as plt
 # importing glob for getting all *.csv files in data folder
 import glob
 
